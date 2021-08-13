268f0f979084bd063313500101023843c2603aa3
TraBee <tra8brown@gmail.com>
2021-08-13 17:41:44 -0600

    added schema and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,30 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+USE employee_db;
+
 DROP TABLE IF EXISTS Department;
 DROP TABLE IF EXISTS Role;
 DROP TABLE IF EXISTS Employee;
 
-CREATE TABLE Department();
+CREATE TABLE Department(
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
 
-CREATE TABLE Role();
+CREATE TABLE Role(
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER, 
+    FOREIGN KEY(department_id) references Department(id)
+);
 
-CREATE TABLE Employee();
+CREATE TABLE Employee(
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER,
+FOREIGN KEY(role_id) references Role(id),
+FOREIGN KEY(manager_id) references Employee(id)
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,10 +1,25 @@
 USE employee_db;
 
 INSERT INTO Department (name)
-VALUES
+VALUES ("Sales"), ('Engineering'), ("Finance"), ("Legal");
 
 INSERT INTO Role (title, salary, department_id)
-VALUES
+VALUES 
+('Sales Lead', 100000, 1 ), 
+('Salesperson', 80000, 1), 
+('Lead Engineer', 150000, 2),
+('Software Engineer', 120000, 2),
+('Accountant', 125000, 3),
+('Legal Team Lead', 250000, 4),
+('Lawyer', 190000, 4);
 
-INSERT INTO Employee (first_name, last_name, manager_id)
+INSERT INTO Employee (first_name, last_name, role_id)
 VALUES
+('John', 'Doe', 1),
+('Mike', 'Chan', 2),
+('Ashley', "Rodriguez", 3),
+('Kevin', "Tupik", 4),
+('Malia', 'Brown', 5),
+('Sarah', 'Lourd', 6),
+('Tom', 'Allen', 7),
+('Tammer', 'Galal', 4);
